42f8a87aa9babda7c70a4382069ace6c5a89b7e7
elkham07 <elkhammamedov@gmail.com>
2026-07-21 15:07:00 +0500

    2 and 3 tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,4 +6,19 @@ def test_search(page: Page):
     page.get_by_role('combobox', name='Search').click()
     page.get_by_role('combobox', name='Search').fill('react')
     page.get_by_role('combobox', name='Search').press('Enter')
-    
+
+
+def test2_search(page: Page):
+    page.goto('https://github.com/')
+    page.get_by_role('button', name='Search').click()
+    page.get_by_role('combobox', name='Search').press('Enter')
+
+
+def test3_search(page: Page):
+    page.goto('https://github.com/')
+    page.get_by_role('button', name='Search').click()
+    page.get_by_role('combobox', name='Search').click()
+    page.get_by_role('combobox', name='Search').fill('qwezxc123456fghjk')
+    page.get_by_role('combobox', name='Search').press('Enter')
+
+
