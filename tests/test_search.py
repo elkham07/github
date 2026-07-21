18ef5a332a4d81ecc75b13913025a41190848c95
elkham07 <elkhammamedov@gmail.com>
2026-07-21 14:59:19 +0500

    1 test

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,4 +2,8 @@ from playwright.sync_api import Page
 
 def test_search(page: Page):
     page.goto('https://github.com/')
+    page.get_by_role('button', name='Search').click()
+    page.get_by_role('combobox', name='Search').click()
+    page.get_by_role('combobox', name='Search').fill('react')
+    page.get_by_role('combobox', name='Search').press('Enter')
     
